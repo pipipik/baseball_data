006f7a29834be1429da378793cab3c0865f7feac
pipipik <hiro.konan913@gmail.com>
2022-07-02 01:39:03 +0900

    投稿を保存

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -8,4 +8,19 @@ class RecordsController < ApplicationController
     @record = Record.new
   end
 
+  def create
+    @record = Record.new(record_params)
+    if @record.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def record_params
+  params.require(:record).permit(:date, :position_id, :result_id).merge(user_id: current_user.id)
+  end
+  
 end
